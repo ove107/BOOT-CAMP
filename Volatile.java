4f20c9828fd02290eef42b49d0570ff1ac20766c
ove107 <40911626+ove107@users.noreply.github.com>
2020-02-20 23:22:20 +0530

    Q1
    Write a programe do to demonstrate the use of volatile keyword.

---
+++ Volatile.java
@@ -0,0 +1,38 @@
+package JavaMultithreading;
+
+import java.util.Scanner;
+
+public class Volatile extends Thread{
+
+        private volatile boolean running = true;
+
+        public void run() {
+            while (running) {
+                System.out.println("Running");
+
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void shutdown() {
+            running = false;
+        }
+    }
+
+    public class App {
+
+        public static void main(String[] args) {
+            Volatile vol = new Volatile();
+            vol.start();
+            // Wait for the enter key
+            System.out.println("Enter something to stop the thread,\nVolatile variable running will be forced to true :");
+            new Scanner(System.in).nextLine();
+            vol.shutdown();
+        }
+    }
+
+}
